0c289e489f57d7229639d39c50ab107304361cb3
Sudiksha Thatipelli <163149118+Sudiksha18@users.noreply.github.com>
2025-03-30 11:16:52 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import SignupPage from './components/SignupPage';
 import LoginPage from './components/LoginPage';
-
+import TeamManagementPage from './components/TeamManagementPage';
+import Settings from './components/Settings';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
-     
+        <Route path="/team" element={<TeamManagementPage />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
   );
